1bbb51be1f690a99264d2598b1e2ffdecddd7213
Ashwin Venkatram <ashwinve@andrew.cmu.edu>
2022-11-04 15:28:10 -0400

    Bugfix: disabled cache_mode in config file

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd ./ODML-Swin-Transformer/
-python main.py --eval --cfg configs/swin/swin_tiny_patch4_window7_224.yaml --resume ./pretrained/swin_tiny_patch4_window7_224.pth --data-path ./data/imagenet/zipped_archives/ --zip
+python main.py --eval --cfg configs/swin/swin_tiny_patch4_window7_224.yaml --resume ./pretrained/swin_tiny_patch4_window7_224.pth --data-path ./data/imagenet/zipped_archives/ --zip --cache-mode no
